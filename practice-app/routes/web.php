6342fa81889731ad4109c74ddaf638fa7dc9c903
Gabteezy <gabrielbumaya1@gmail.com>
2024-03-13 00:39:20 +0800

    updated

--- practice-app/routes/web.php
+++ practice-app/routes/web.php
@@ -21,40 +21,40 @@ Route::get('/homepage', function () {
     return view('homepage');
 })->name("homepage");
 
-Route::get('/about', function () {
-    return view('about');
-})->name("about");
+// Route::get('/about', function () {
+//     return view('about');
+// })->name("about");
 
-Route::get('/login', function () {
-    return view('login');
-})->name("login");
+// Route::get('/login', function () {
+//     return view('login');
+// })->name("login");
 
-Route::get('/services', function () {
-    return view('services');
-})->name("services");
+// Route::get('/services', function () {
+//     return view('services');
+// })->name("services");
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name("contact");
+// Route::get('/contact', function () {
+//     return view('contact');
+// })->name("contact");
 
 
-Route::name('dashboard.')->group(function () {
-    Route::get('/admin', function () {
-       return view("account");
-    })->name('admin');
-    Route::get('/user', function () {
-       return view("account");
-    })->name('user');
-});
+// Route::name('dashboard.')->group(function () {
+//     Route::get('/admin', function () {
+//        return view("account");
+//     })->name('admin');
+//     Route::get('/user', function () {
+//        return view("account");
+//     })->name('user');
+// });
 
-Route::name('services.')->group(function () {
-    Route::get('/service1', function () {
-       return view("service1");
-    })->name('service1');
-    Route::get('/service2', function () {
-       return view("service2");
-    })->name('service2');
-    Route::get('/service3', function () {
-       return view("service3");
-    })->name('service3');
-});
+// Route::name('services.')->group(function () {
+//     Route::get('/service1', function () {
+//        return view("service1");
+//     })->name('service1');
+//     Route::get('/service2', function () {
+//        return view("service2");
+//     })->name('service2');
+//     Route::get('/service3', function () {
+//        return view("service3");
+//     })->name('service3');
+// });
